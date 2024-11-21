af4a367dd679256aab77640cd49427c7dc823276
chris-mathevet <chris.mathevet@etu.univ-orleans.fr>
2024-11-21 16:39:26 +0100

    trigger

--- assets/SQL/trigger/trigger.sql
+++ assets/SQL/trigger/trigger.sql
@@ -77,14 +77,16 @@ BEGIN
     declare heureDebutUnCours DECIMAL(3,1) DEFAULT 0.0;
     declare dureeUnCours TINYINT DEFAULT 1;
 
-    declare vraiDureeCours TINYINT DEFAULT dureeCours;
+    declare vraiDureeCours TINYINT DEFAULT 1;
 
     declare vraiHeureDebutUnCours DECIMAL(3,1) DEFAULT 0.0;
+    declare calculHeure DECIMAL(3,1) DEFAULT 0.0;
 
     declare cptCoursUneHeure TINYINT DEFAULT 0;
 
     declare res BOOLEAN DEFAULT TRUE;
     declare fini BOOLEAN DEFAULT FALSE;
+
     declare lesCours cursor for
         select heureDebutCours, duree from RESERVATION NATURAL JOIN COURS where dateCours=dateCours and idPoney=idPoney order by heureDebutCours;
     declare continue handler for not found set fini = TRUE;
@@ -93,7 +95,7 @@ BEGIN
         set vraiDureeCours = 3;
     end if;
     open lesCours;
-    while not (fini and res) do
+    while (not fini and res) do
         fetch lesCours into heureDebutUnCours, dureeUnCours;
 
         if not fini then
@@ -101,11 +103,13 @@ BEGIN
                 set vraiHeureDebutUnCours = heureDebutUnCours;
             end if;
 
-            if dureeUnCours = 1 and vraiHeureDebutUnCours+dureeUnCours*(cptCoursUneHeure) = heureDebutUnCours then
+            set calculHeure = vraiHeureDebutUnCours+cptCoursUneHeure;
+            if dureeUnCours = 1 and (cptCoursUneHeure = 0 or calculHeure = heureDebutUnCours) then
                 set cptCoursUneHeure = cptCoursUneHeure + 1;
             elseif dureeUnCours = 2 then
                 set dureeUnCours = 3; -- Prendre en compte une heure de repoos si le cours fait 2h
                 set cptCoursUneHeure = 0;
+                set vraiHeureDebutUnCours = heureDebutUnCours;
             end if;
 
             if cptCoursUneHeure != 0 then
@@ -126,23 +130,37 @@ create or replace trigger repos before insert on RESERVATION for each row
     declare res BOOLEAN DEFAULT FALSE;
     declare dureeCours TINYINT DEFAULT 1;
     declare mes VARCHAR(100) DEFAULT "";
+    declare countRes INT DEFAULT 0;
+    
     select duree into dureeCours from COURS where idCours = new.idCours;
-    set res = (select poneyDispo(new.idPoney, new.idCours,new.usernameMoniteur,new.dateCours,new.heureDebutCours,dureeCours));
+    select IFNULL(count(new.idPoney),0) into countRes from RESERVATION where idPoney = new.idPoney and dateCours = new.dateCours;
+
+    if countRes > 0 then
+        set res = (select poneyDispo(new.idPoney, new.idCours, new.usernameMoniteur, new.dateCours, new.heureDebutCours, dureeCours));
+    else
+        set res = TRUE;
+    end if;
 
     if not res then
-        set mes = concat ("inscription impossible" , new.idPoney , "n'est pas disponible") ;
+        set mes = concat ("inscription impossible " , new.idPoney , " n'est pas disponible") ;
         signal SQLSTATE '45000' set MESSAGE_TEXT = mes ;
     end if ;
 end |
 delimiter ;
 
-INSERT INTO REPRESENTATION (idCours, usernameMoniteur, dateCours, heureDebutCours) VALUES
-(1, 'moniteur2', '2023-12-01', 11.0), -- Valide
-(1, 'moniteur2', '2023-12-01', 10.5), -- Invalide car chevauchement
--- (1, 'moniteur2', '2023-12-01', 9.0), -- Valide car avant
-(1, 'moniteur2', '2023-12-01', 12.0); -- Invalide suite au court de 10h et 11h
+-- INSERT INTO REPRESENTATION (idCours, usernameMoniteur, dateCours, heureDebutCours) VALUES
+-- (1, 'moniteur2', '2023-12-01', 10.5), -- Invalide car chevauchement
+-- (1, 'moniteur2', '2023-12-01', 11.0), -- Valide
+-- -- (1, 'moniteur2', '2023-12-01', 9.0), -- Valide car avant
+-- (1, 'moniteur2', '2023-12-01', 12.0); -- Invalide suite au court de 10h et 11h
 
-INSERT INTO RESERVATION (idCours, usernameMoniteur, dateCours, heureDebutCours, usernameClient, idPoney) VALUES
-(1, 'moniteur2', '2023-12-01', 11.0, 'client1', 1), -- Valide
-(1, 'moniteur2', '2023-12-01', 10.5, 'client1', 1),
-(1, 'moniteur2', '2023-12-01', 12.0, 'client1', 1);
+-- INSERT INTO RESERVATION (idCours, usernameMoniteur, dateCours, heureDebutCours, usernameClient, idPoney) VALUES
+-- (1, 'moniteur2', '2023-12-01', 11.0, 'client1', 1); -- Valide
+-- INSERT INTO RESERVATION (idCours, usernameMoniteur, dateCours, heureDebutCours, usernameClient, idPoney) VALUES
+-- (1, 'moniteur2', '2023-12-01', 10.5, 'client1', 1);
+-- INSERT INTO RESERVATION (idCours, usernameMoniteur, dateCours, heureDebutCours, usernameClient, idPoney) VALUES
+-- (1, 'moniteur2', '2023-12-01', 12.0, 'client1', 1);
+
+select poneyDispo(1, 1, 'moniteur2', '2023-12-01', 11.0, 1);
+select poneyDispo(1, 1, 'moniteur2', '2023-12-01', 10.5, 1);
+select poneyDispo(1, 1, 'moniteur2', '2023-12-01', 12.0, 1);
