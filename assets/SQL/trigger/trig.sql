7c088aba487d3128812f1f33ad0e4afbb5eaa39b
Renaudin clement <cronisosdev@gmail.com>
2024-11-24 20:21:38 +0100

    recuperation du trigger corriger par chris

--- assets/SQL/trigger/trig.sql
+++ assets/SQL/trigger/trig.sql
@@ -1,38 +1,36 @@
---procesdure question 5
+-- TRUC CLEM
+-- procesdure question 5
 
 delimiter |
-create or replace procedure proc_heure_avant (in heure_deb DECIMAL, in dateCourss DATE, in usernameMoniteurs VARCHAR(32), in duree_existant INT,out est_chevaucher BOOLEAN) 
-
+create or replace function proc_heure_avant (heure_deb DECIMAL(3,1), dateCourss DATE, usernameMoniteurs VARCHAR(32), duree_existant TINYINT) 
+returns BOOLEAN
 begin
-    declare identifant_cours INT;
-    declare durees DECIMAL ;
-    declare heureDebutCourss DECIMAL;
-    declare fini BOOLEAN default false ;
-    
-    declare lesCours cursor for 
-        select idCours 
-        from REPRESENTATION 
+    declare identifant_cours INT default 0;
+    declare durees TINYINT default 0;
+    declare heureDebutCourss DECIMAL(3,1) default 0.0;
+    declare fini BOOLEAN default false;
+    declare est_chevaucher boolean default false;
+
+    declare lesCours cursor for
+        select idCours, duree,heureDebutCours
+        from REPRESENTATION natural join COURS
         where dateCours = dateCourss and usernameMoniteur = usernameMoniteurs and heureDebutCours < heure_deb;
-    
+
     declare continue handler for not found set fini = true;
     open lesCours ;
-    while not fini DO
-        fetch lesCours into identifant_cours ;
-        if not fini then
-            select duree,heureDebutCours into durees,heureDebutCourss from COURS where idCours = identifant_cours;
-
-
-            if heureDebutCourss + durees < heure_deb + duree_existant then
+    while (not fini and not est_chevaucher) DO
+        fetch lesCours into identifant_cours, durees,heureDebutCourss;
+        if (not fini and not est_chevaucher) then
+            if heureDebutCourss + durees < heure_deb then
                    set est_chevaucher = true;
-                   
             end if ;
         end if ;
     end while ;
-    
-    close lesCours ;
-    select est_chevaucher;
 
-end |   
+    close lesCours ;
+    return est_chevaucher;
+
+end |
 delimiter ;
 
 
@@ -41,22 +39,17 @@ delimiter ;
 delimiter |
 create or replace trigger court_deja_present_1h_avant_representer before insert on REPRESENTATION for each row
 begin
-    declare duree_existant INT ;
+    declare duree_existant TINYINT default 0;
     declare est_chevaucher boolean default false ;
-    declare mes varchar (100) ;
+    declare mes varchar (100) default "";
 
-    
     select duree into duree_existant from COURS where  idCours = new.idCours;
 
-    call proc_heure_avant (new.heureDebutCours , new.dateCours , new.usernameMoniteur , duree_existant , est_chevaucher) ;
-    
-    if est_chevaucher = true then
+    set est_chevaucher = (select proc_heure_avant (new.heureDebutCours , new.dateCours , new.usernameMoniteur , duree_existant)) ;
+
+    if est_chevaucher then
         set mes = concat ( 'un precedent cours chevauche le nouveau' ) ;
         signal SQLSTATE '45000' set MESSAGE_TEXT = mes ;
     end if ;
-    
-    
 end |
 delimiter ;
-
-
