7f17a5bb68ff2f6fd5fef4ae1af6e2aeabd7268c
julian-marques <juliandronefpv@gmail.com>
2024-12-01 21:30:26 +0100

    exemple bd + selection + insertion fait

--- utils/connexionBD.php
+++ utils/connexionBD.php
@@ -1,3 +1,27 @@
 <?php
-
+// base de base 
 // code de connexion a une base de donnée
+
+session_start();
+$host ="localhost";
+$table =" ";
+$user = ' ';
+$mdp = ' ';
+$bdd = new PDO('mysql:host='.$host.';dbname='.$table.'', $user, $mdp);
+
+
+// requete select exemple:
+$requser = $bdd-> prepare("SELECT * FROM USER");
+$requser->execute();
+$userinfo = $requser->fetch();
+
+
+echo "<pre>";
+print_r($userinfo);
+
+echo "</pre>";
+
+// requete insert exemple:
+$insertmbr = $bdd->prepare("INSERT INTO user(pseudo, mdp, type) VALUES(?, ?, ?)");
+$insertmbr->execute(array("troll", "1234", "admin"));
+
