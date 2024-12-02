15408817a7151acd031af2138c20a15b2d2a5f16
julian-marques <juliandronefpv@gmail.com>
2024-12-02 15:24:34 +0100

    page initial moniteur (admin) + header

--- assets/affichage/header.php
+++ assets/affichage/header.php
@@ -2,42 +2,64 @@
 
 if($_SESSION["connecte"]["role"] ==="client"){
     
-    echo '
-    <header>
-        <nav>
-        <ul>    
-            <li>
-            <a href="#planning">Planning</a>
-            </li>
-            
-            <li>
-            <a href="#reserverCours">Reserver cours</a>
-            </li>
-            
-            <li>
-            <a href="#parametre">Parametre</a>
-            </li>
-        </nav>
-    </header>';
+    echo '<header>
+            <nav>
+            <ul>    
+                <li>
+                <a href="#planning">Planning</a>
+                </li>
+                
+                <li>
+                <a href="#reserverCours">Reserver cours</a>
+                </li>
+                
+                <li>
+                <a href="#parametre">Parametre</a>
+                </li>
+            </nav>
+        </header>';
 }
-elseif($_SESSION["connecte"]["role"] ==="client"){
-echo 
-    '<header>
-        <nav>
-        <ul>    
-            <li>
-            <a href="#">Planning</a>
-            </li>
-            
-            <li>
-            <a href="#">Reserver cours</a>
-            </li>
-            
-            <li>
-            <a href="#">Parametre</a>
-            </li>
-        </nav>
-    </header>';
+elseif($_SESSION["connecte"]["role"] ==="moniteur"){
+    echo 
+        '<header>
+            <nav>
+            <ul>    
+                <li>
+                    <a href="#planning">Planning</a>
+                </li>
+                
+                <li>
+                    <a href="#creerCours">Creer cours</a>
+                </li>
+                
+                <li>
+                    <a href="#parametre">Parametre</a>
+                </li>
+            </nav>
+        </header>';
+}
+elseif($_SESSION["connecte"]["role"] === "admin"){
+    echo 
+        '<header>
+            <nav>
+            <ul>    
+                <li>
+                    <a href="#planning">Planning</a>
+                </li>
+                
+                <li>
+                    <a href="#creerCours">Creer cours</a>
+                </li>
+                
+                <li>
+                    <a href="#parametre">Parametre</a>
+                </li>
+
+                <li>
+                    <a href="administration.php">administration</a>
+                </li>
+            </nav>
+        </header>';
 }
     
 ?>
--- page/moniteur.php
+++ page/moniteur.php
@@ -5,19 +5,101 @@ require_once "../utils/connexionBD.php";
 // require_once __DIR__ . "/../../utils/connexion.php";
 
 require_once "../utils/annexe.php";
-
-if(isset($_SESSION["connecte"])){   
-    
-    echo "<pre>";
-    print_r($_SESSION);
-    echo "</pre>";
-
-}
-else{
-    header("Location : 404.php");
-    echo "vous avez pas acces a cette page";
+if(!isset($_SESSION["connecte"])){
+    header("Location: ../");
 }
 
 
+?>
 
-echo "page moniteur / admin";
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grand Galop</title>
+    <link rel="stylesheet" href="../assets/style/style.css">
+    <link rel="stylesheet" href="../assets/style/header.css">
+    <link rel="stylesheet" href="../assets/style/styleSousPage.css">
+</head>
+    <body>
+        <header>
+            <h1>GRAND GALOP</h1>
+            
+            <?php
+                //
+                if(isset($_SESSION["connecte"])){
+                    echo '<div class="auth-buttons">
+                            <p>'.$_SESSION["connecte"]["prenom"].'</p>
+                    
+                            <button onclick="location.href=\'../utils/logout.php\';" class="affichelogin">Logout</button>
+                        </div>';
+                    include "../assets/affichage/header.php";
+                }
+                else{
+                    echo '<div class="auth-buttons">
+                        <button class="affichelogin">Login</button>
+                        <button class="afficheSignIn">Sign In</button>
+                    </div>';
+                }
+            ?>
+        </header>
+        
+        <main class="container">
+
+            <section id="planning" class="sectionPage">
+                <h2 class="titreSection"> Planning</h2>
+                <section class="gauche-section gauche">
+                    <!-- ? a faire calendrier -->
+                    <figure class="image-block">
+                        <img src="../assets/images/cal.png" alt="cal" class="planning">
+                    </figure>
+                </section>
+                
+                <section class="droite-section droite">
+                    <figure class="image-block">
+                        <img src="../assets/images/cheval2.png" alt="Cheval" class="cheval-image-droite">
+                    </figure>
+                </section>
+            </section>
+                    
+            <section id="creerCours" class="sectionPage">
+                <h2 class="titreSection"> Creer un cours</h2>
+            
+                <section class="gauche-section gauche">
+                    <!-- ? a faire calendrier -->
+                    <figure class="image-block">
+                        <img src="../assets/images/cal.png" alt="cal" class="planning">
+                    </figure>
+                </section>
+                
+                <section class="droite-section droite">
+                    <figure class="image-block">
+                            <img src="../assets/images/cheval2.png" alt="Cheval" class="cheval-image-droite">
+                    </figure>
+                </section>
+            </section>
+
+                                
+            <section id="parametre" class="sectionPage">
+                <h2 class="titreSection"> Gérer mon dossier</h2>
+            
+                <section class="gauche-section gauche">
+                    <!-- ? a faire calendrier -->
+                    <figure class="image-block">
+                        <img src="../assets/images/cal.png" alt="cal" class="planning">
+                    </figure>
+                </section>
+                
+                <section class="droite-section droite">
+                    <figure class="image-block">
+                            <img src="../assets/images/cheval2.png" alt="Cheval" class="cheval-image-droite">
+                    </figure>
+                </section>
+            </section>
+
+        </main>
+    </body>
+
+</html>
