d793f58c5c63bed4906b5b06ea1c8d62f9bd5919
julian-marques <juliandronefpv@gmail.com>
2024-12-02 19:08:39 +0100

    suppression de poney

--- utils/removePoney.php
+++ utils/removePoney.php
@@ -7,13 +7,25 @@ if(!isset($_SESSION["connecte"]) OR $_SESSION["connecte"]["role"] !== "admin"){
 if(isset($_GET["idPoney"]))
 {
 
+    $reqUser = $bdd->prepare("SELECT * FROM RESERVATION WHERE idPoney = ? ");
+    $reqUser->execute([$_GET["idPoney"]]);
+    $lesReserv = $reqUser->fetchAll();
+    echo $userExist ;
+    echo "<pre>";
+    foreach ($lesReserv as $reservation) {
+        print_r( $reservation);
+        $sql = "DELETE FROM RESERVATION WHERE idCours = ? and usernameMoniteur = ? and dateCours = ? and heureDebutCours = ? and usernameClient = ? and idPoney = ?";
+        $stmt = $bdd->prepare($sql);
+        $stmt ->execute([$reservation["idCours"], $reservation["usernameMoniteur"], $reservation["dateCours"], $reservation["heureDebutCours"], $reservation["usernameClient"], $reservation["idPoney"]]);
+    }
+    echo "</pre>";
     
     // ! reservation a faire
     $sql = "DELETE FROM PONEY WHERE idPoney= ? ";
     $stmt = $bdd->prepare($sql);
-    $stmt ->execute([$_GET["idPoney"]]);
+    $stmt ->execute([$reservation["idPoney"]]);
 }
 
-
+header("Location: ../page/administration.php");
 
 ?>
